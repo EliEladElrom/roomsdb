6b6fd0b557dcef3405f524c4df36dbb858f25c0a
Elad Elrom <elad.ny@gmail.com>
2013-07-21 09:50:55 -0400

    adding mongodb

--- roomsdb_sample/getitems.js
+++ roomsdb_sample/getitems.js
@@ -4,21 +4,22 @@
  * @author Elad Elrom <elad.ny...gmail.com>
  */
 
-function getItems(data, dbconnectorCallBackToRooms) {
+// mySQL example
 
-    var sqlString = "SELECT * FROM [database].[table] WHERE [some-condition] != '' LIMIT 1";
-
-    this.sqlCommand(sqlString,
-        function (rows) {
-            var vo = new VO(rows[0].id, rows[0].name);
-            dbconnectorCallBackToRooms(data,vo);
-        }
-    );
-}
+'use strict';
 
 function VO(id, name) {
-    this.id = id;
-    this.name = name;
+  this.id = id;
+  this.name = name;
 }
 
-module.exports.getItems = getItems;
+function getitems(data, dbconnectorCallBackToRooms) {
+  var sqlString = "SELECT * FROM test.users WHERE name != ''";
+  this.sqlCommand(sqlString,
+    function (rows) {
+      var vo = new VO(rows[0].id, rows[0].name);
+      dbconnectorCallBackToRooms(data, vo);
+    });
+}
+
+module.exports.getitems = getitems;
